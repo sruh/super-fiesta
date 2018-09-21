1ddc53da2ddf4b4a3f47a68cd4c15cbbf06dc80b
Stefan Ruh <ruh@abgeordnetenwatch.de>
2018-09-21 19:50:18 +0200

    Add a bunch of stuff

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# super-fiesta
+# Wedding Invitation Page
---
+++ contact.php
@@ -0,0 +1,50 @@
+<?php
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Please complete the form.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        $recipient = "none@example.com";
+
+        // Set the email subject.
+        $subject = "RSVP from $name";
+
+        // Build the email content.
+        $email_content = "Firma: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Nachricht:\n$message\n";
+
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank you, your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Your message could not be sent due to an error.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        #echo "Ihre Nachricht konnte aufgrund eines Fehlers nicht verschickt werden.";
+    }
+
+?>
