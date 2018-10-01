96be0bcdabfce11f486230e501309765ce8578d4
Stefan Ruh <ruh@abgeordnetenwatch.de>
2018-10-01 18:21:21 +0200

    Improve i18n

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,82 +1,92 @@
 // Credit: https://codeburst.io/easy-i18n-in-10-lines-of-javascript-poc-eb9e5444d71e
 function i18n(template) {
-    for (var info = i18n.db[i18n.locale][template.join('\x01')],
+    for (var info = i18n.db[i18n.locale][template],
             out = [info.t[0]],
             i = 1, length = info.t.length; i < length; i++) out[i] = arguments[1 + info.v[i - 1]] + info.t[i];
     return out.join('');
 }
-if (navigator.language.includes('de')) {
-    i18n.locale = 'de';
-} else if (navigator.language.includes('bg')) {
-    i18n.locale = 'bg';
-} else i18n.locale = 'en';
 i18n.db = {
     "en": {
-        "Eli &amp; Stefan are getting married": {
+        "title": {
             "t": ["Eli &amp; Stefan are getting married"],
             "v": []
         },
-        "When?": {
+        "when": {
             "t": ["When?"],
             "v": []
         },
-        "Where?": {
+        "where": {
             "t": ["Where?"],
             "v": []
         },
-        "What else?": {
+        "what-else": {
             "t": ["What else?"],
             "v": []
         },
-        "RSVP right away": {
+        "rsvp-now": {
             "t": ["RSVP right away"],
             "v": []
         }
     },
     "de": {
-        "Eli &amp; Stefan are getting married": {
+        "title": {
             "t": ["Eli &amp; Stefan heiraten"],
             "v": []
         },
-        "When?": {
+        "when": {
             "t": ["Wann?"],
             "v": []
         },
-        "Where?": {
+        "where": {
             "t": ["Wo?"],
             "v": []
         },
-        "What else?": {
+        "what-else": {
             "t": ["Was noch?"],
             "v": []
         },
-        "RSVP right away": {
+        "rsvp-now": {
             "t": ["Sofort antworten"],
             "v": []
         }
 
     },
     "bg": {
-        "Eli &amp; Stefan are getting married": {
+        "title": {
             "t": ["Ели и Щефан се женят!"],
             "v": []
         },
-        "When?": {
+        "when": {
             "t": ["Кога?"],
             "v": []
         },
-        "Where?": {
+        "where": {
             "t": ["Къде?"],
             "v": []
         },
-        "What else?": {
+        "what-else": {
             "t": ["Какво друго?"],
             "v": []
         },
-        "RSVP right away": {
+        "rsvp-now": {
             "t": ["Test"],
             "v": []
         }
 
     }
 };
+function i18n_load(locale){
+  if (locale == null){
+    if (navigator.language.includes('de')) {
+        i18n.locale = 'de';
+    } else if (navigator.language.includes('bg')) {
+        i18n.locale = 'bg';
+    } else i18n.locale = 'en';
+  }
+  else {
+    i18n.locale = locale;
+  }
+  var elements = document.querySelectorAll('[data-i18n-key]');
+  Array.from(elements, (el) => {el.innerHTML = i18n(`${el.getAttribute('data-i18n-key')}`)});
+}
+i18n_load();
